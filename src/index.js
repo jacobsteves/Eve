cb8ea616ab0ad1405b42b79f120697d711d90054
Jacob Steves <jgsteves@uwaterloo.ca>
2017-06-10 16:27:19 -0400

    react/redux starter

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/default */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router';
+import { AppContainer } from 'react-hot-loader';
+import Root from './components/Root';
+
+import configureStore from './store/configureStore';
+require('./favicon.ico'); // Tell webpack to load favicon.ico
+import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import { syncHistoryWithStore } from 'react-router-redux';
+
+const store = configureStore();
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store);
+
+
+render(
+  <AppContainer>
+    <Root store={store} history={history} />
+  </AppContainer>,
+  document.getElementById('app')
+);
+
+if (module.hot) {
+  module.hot.accept('./components/Root', () => {
+    const NewRoot = require('./components/Root').default;
+    render(
+      <AppContainer>
+        <NewRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('app')
+    );
+  });
+}
